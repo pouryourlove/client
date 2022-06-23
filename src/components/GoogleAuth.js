46bb185a95743c099c8dc1f373d6676604bef954
Juyeon Lee <lqu134@naver.com>
2022-06-23 17:23:15 +0900

    state added

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "1075703354356-0hig5f92t0ng78v6u6882rqaedimra2u.apps.googleusercontent.com",
+          scope: "email",
+          plugin_name: "streamy",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+    });
+  }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div> I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;

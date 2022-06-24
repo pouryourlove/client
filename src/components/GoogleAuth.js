5d79028cf4e3f24f4f88a2ecf2ed69b8dbc1110b
Juyeon Lee <lqu134@naver.com>
2022-06-24 16:01:56 +0900

    sign in and out

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -19,13 +19,35 @@ export class GoogleAuth extends Component {
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I don't know if we are signed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div> I am signed in!</div>;
+      return (
+        <button onClick={this.onSignOut} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button onClick={this.onSignIn} className="ui red google button">
+          <i className="google icon" />
+          Sign in with google
+        </button>
+      );
     }
   }
 
